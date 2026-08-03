f105e2b95c306e812f4557d1d960d9f667d2d227
Sravanika1987 <kollusravanika@gmail.com>
2026-08-03 09:14:37 +0530

    Create specialTypes.ts

---
+++ week1/specialTypes.ts
@@ -0,0 +1,13 @@
+let value: any = 10;
+console.log(value);
+
+value = "Hello";
+console.log(value);
+
+let dta: unknown = "TypeScript";
+
+function printMessage(): void {
+    console.log("Learning TypeScript");
+}
+
+printMessage();
