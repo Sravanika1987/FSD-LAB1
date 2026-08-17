c235a3647757614aad2b17780b8ec3e3dbf34563
Sravanika1987 <kollusravanika@gmail.com>
2026-08-17 11:37:00 +0530

    Create 4_query_parameter.js

---
+++ week1/week-2/week-3/wewek-5/4_query_parameter.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+app.get("/search", (req, res) => {
+    const keyword = req.query.keyword || "none";
+    const page = req.query.page || "1";
+
+    res.json({
+        message: "Query parameters received",
+        keyword: keyword,
+        page: page
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Query parameter server running at http://localhost:${PORT}`);
+});
