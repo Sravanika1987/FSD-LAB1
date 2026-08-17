c808b2ec12008ddc6745d0049b0736c7a597fcc3
Sravanika1987 <kollusravanika@gmail.com>
2026-08-17 11:39:06 +0530

    Create 2_json.js

---
+++ week1/week-2/week-3/wewek-5/week-6/2_json.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+
+const PORT = 3001;
+app.use(express.json());
+
+app.get("/profile", (req, res) => {
+    res.json({
+        name: "Bhavana",
+        branch: "AI & ML",
+        semester: "III B.Tech"
+    });
+});
+
+app.post("/profile", (req, res) => {
+    const profile = req.body;
+
+    res.status(201).json({
+        message: "JSON data received successfully",
+        data: profile
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`JSON server running at http://localhost:${PORT}`);
+});
